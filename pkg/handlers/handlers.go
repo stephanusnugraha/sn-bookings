d5d2b19c89c2972270a8b12b56fc28394d3f0347
Stephanus Nugraha <mail@stephanusnugraha.com>
2023-09-09 11:48:14 +0700

    add search-availability-json

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/stephanusnugraha/sn-bookings/pkg/config"
 	"github.com/stephanusnugraha/sn-bookings/pkg/models"
 	"github.com/stephanusnugraha/sn-bookings/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -76,6 +78,27 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	start := r.Form.Get("start")
 	end := r.Form.Get("end")
 	w.Write([]byte(fmt.Sprintf("start date is %s and end date is %s", start, end)))
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+// AvailabilityJSON handle request for availability and send JSON Response
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK:      false,
+		Message: "Available!",
+	}
+
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 // Contact renders the contact page

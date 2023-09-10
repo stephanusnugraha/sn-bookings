61e5c089d8e1af104efa79e6fd33ca3e2e7e2cd8
Stephanus Nugraha <mail@stephanusnugraha.com>
2023-09-10 15:36:53 +0700

    add forms, remove client side validation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/stephanusnugraha/sn-bookings/internal/config"
+	"github.com/stephanusnugraha/sn-bookings/internal/forms"
 	"github.com/stephanusnugraha/sn-bookings/internal/models"
 	"github.com/stephanusnugraha/sn-bookings/internal/render"
 	"log"
@@ -55,7 +56,14 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservation renders the make a reservation page and displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+	})
+}
+
+// PostReservation handles the posting of a reservation form
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+
 }
 
 // Generals renders the room page

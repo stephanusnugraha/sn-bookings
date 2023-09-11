580f749a3b569276c9a548a89b1f1e66cea0d111
Stephanus Nugraha <mail@stephanusnugraha.com>
2023-09-11 17:50:09 +0700

    add page reservation-summary and put data from session

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"github.com/alexedwards/scs/v2"
 	"github.com/stephanusnugraha/sn-bookings/internal/config"
 	"github.com/stephanusnugraha/sn-bookings/internal/handlers"
+	"github.com/stephanusnugraha/sn-bookings/internal/models"
 	"github.com/stephanusnugraha/sn-bookings/internal/render"
 	"log"
 	"net/http"
@@ -18,6 +20,9 @@ var session *scs.SessionManager
 
 // main is the main function
 func main() {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
 	// change this to true when in production
 	app.InProduction = false
 

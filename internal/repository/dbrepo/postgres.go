ca4c9ad585da630fbe4f4e4b13875a9580fd0e12
Stephanus Nugraha <mail@stephanusnugraha.com>
2023-09-20 16:47:04 +0700

    add search availability by room

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -64,3 +64,29 @@ func (m *postgresDBRepo) InsertRoomRestriction(res models.RoomRestriction) error
 
 	return nil
 }
+
+// SearchAvailabilityByDates returns true if availability exists dor roomID, and false if no avail
+func (m *postgresDBRepo) SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var numRows int
+
+	query := `
+		select count(id)
+			from room_restrictions
+			where room_id = $1 
+			    and $2 < end_date and $3 > start_date;`
+
+	row := m.DB.QueryRowContext(ctx, query, roomID, start, end)
+	err := row.Scan(&numRows)
+	if err != nil {
+		return false, err
+	}
+
+	if numRows == 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,10 +1,14 @@
 package repository
 
-import "github.com/stephanusnugraha/sn-bookings/internal/models"
+import (
+	"github.com/stephanusnugraha/sn-bookings/internal/models"
+	"time"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }

027770e067d5fb7f4941441e5024b7dcc6150d16
Stephanus Nugraha <mail@stephanusnugraha.com>
2023-09-23 23:16:44 +0700

    add choose-room, connect handler to db when make-reservation

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -103,7 +103,7 @@ func (m *postgresDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]
 			where r.id not in
 				(select room_id from room_restrictions rr where $1 < rr.end_date and $2 > rr.start_date);`
 
-	rows, err := m.DB.QueryContext(ctx, query)
+	rows, err := m.DB.QueryContext(ctx, query, start, end)
 	if err != nil {
 		return rooms, err
 	}
